d67016c141b42b37c44adbb53accd00ea508207f
jlinck <jlinck@mitchellhumphrey.com>
2022-07-15 12:56:26 -0500

    Add project files.

---
+++ BindableLayoutRepo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BindableLayoutRepo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BindableLayoutRepo/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BindableLayoutRepo;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
